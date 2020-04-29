ad243eed75fb8eba86fa4e6109af5ecabb488031
Andrew Axelrod <aaxelrod@Andrews-MBP.hsd1.il.comcast.net>
2020-04-29 14:06:16 -0500

    Fixed bugs

--- public/common/clean-template/index.js
+++ public/common/clean-template/index.js
@@ -1,58 +1,70 @@
-const cleanTemplate = () => {
-  const clean = () => {
-    $(`.collection-table`)
-      .closest(`td`)
-      .children(`.euiToolTipAnchor`)
-      .remove();
+import _ from "lodash";
+import "../jquery-plugins/observer";
+
+const cleanTemplate = (showPopover) => {
+  const clean = (showPopover) => {
+    $(`.collection-table`).closest(`td`).children(`.euiToolTipAnchor`).remove();
 
     $(`.collection-table`)
       .closest(`.kbnDocTableCell__dataField`)
-      .addClass('white-space-normal');
+      .addClass("white-space-normal");
 
     $(`.collection-table`)
       .closest(`.kbnDocViewer__value`)
-      .addClass('white-space-normal');
+      .addClass("white-space-normal");
 
     $(`.collection-table-no-filter`)
-      .closest('.kbnDocTableCell__dataField')
-      .children('.kbnDocTableCell__filter')
+      .closest(".kbnDocTableCell__dataField")
+      .children(".kbnDocTableCell__filter")
       .empty();
 
     $(`.collection-table-no-filter`)
       .closest(`tr[data-test-subj]`)
-      .children('.kbnDocViewer__buttons')
-      .children('span')
+      .children(".kbnDocViewer__buttons")
+      .children("span")
       .children(`button:not([data-test-subj='toggleColumnButton'])`)
-      .attr('disabled', true);
-  };
+      .attr("disabled", true);
 
-  const targetNode = document.getElementById('kibana-app');
-  const config = { attributes: true, childList: true, subtree: true };
-  const callback = function(mutationsList, observer) {
-    for (let mutation of mutationsList) {
-      if (
-        mutation &&
-        mutation.target &&
-        mutation.target.className &&
-        mutation.target.className.indexOf &&
-        (mutation.target.className.indexOf('kbnDocTableCell__dataField') !== -1 ||
-          mutation.target.className.indexOf('kbnDocTableDetails__row') !== -1)
-      ) {
-        clean();
-      }
+    if (showPopover) {
+      $(`.collection-object-table`)
+        .closest(".kbnDocTableCell__dataField")
+        .children(".kbnDocTableCell__filter")
+        .children(`.kbnDocTableRowFilterButton`)
+        .addClass("tippy-filter-button");
+
+      $(`.collection-object-table`)
+        .closest(`tr[data-test-subj]`)
+        .children(".kbnDocViewer__buttons")
+        .children("span")
+        .children(`button:not([data-test-subj='toggleColumnButton'])`)
+        .closest(`.euiToolTipAnchor`)
+        .addClass("tippy-filter-button");
     }
   };
 
-  if (targetNode) {
-    const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-  }
+  $("#kibana-app").observe(
+    ".kbnDocTableCell__dataField",
+    _.throttle(() => {
+      setTimeout(() => {
+        console.log("kbnDocTableCell__dataField");
+        clean(showPopover);
+      }, 0); // Increment to 1000 if doens't work.
+    }, 1000)
+  );
+
+  $("#kibana-app").observe(
+    ".kbnDocViewerTable tr",
+    _.throttle(() => {
+      console.log("kbnDocTableDetails__row");
+      clean(showPopover);
+    }, 1000)
+  );
 };
 
-export default () => {
+export default (showPopover = false) => {
   setTimeout(() => {
-    if ($('.application.tab-discover').length) {
-      cleanTemplate();
+    if ($(".application.tab-discover").length) {
+      cleanTemplate(showPopover);
     }
   }, 0);
 };
