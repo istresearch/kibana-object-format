855446fd44360cb5011a8f5dfd08dd54115fba20
Andrew Axelrod <aaxelrod@Andrews-MBP.hsd1.il.comcast.net>
2020-06-04 10:55:09 -0500

    Fixed FF scroll bug

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ export default function(kibana) {
         'plugins/kibana_object_format/hacks/field_mapper_hack',
         'plugins/kibana_object_format/hacks/custom_filter_bootstrap',
         'plugins/kibana_object_format/hacks/object_filter',
+        'plugins/kibana_object_format/hacks/scroll_bug',
       ],
       fieldFormats: ['plugins/kibana_object_format/field_formats/object/register'],
       uiSettingDefaults: {
